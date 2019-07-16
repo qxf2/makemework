0b5d0d6514fd68648c38e7a540d8dbab2792fe0a
Sravanti25 <sravanti.t25@gmail.com>
2019-07-16 11:36:38 +0530

    added return cart_quantity in the get_current_cart_quantity method in Product_Object.py

--- page_objects/Product_Object.py
+++ page_objects/Product_Object.py
@@ -119,6 +119,7 @@ class Product_Object():
         self.conditional_write(True,
         positive="The cart currently has %d items"%self.CART_QUANTITY,
         negative="")
+        return cart_quantity
 
     def add_product(self,product_name):
         "Add the lowest priced product with the filter condition in name"
