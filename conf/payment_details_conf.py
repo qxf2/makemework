d1ab77a6d4503412edb80608095eefdc6c93b804
Sravanti25 <sravanti.t25@gmail.com>
2019-07-22 09:55:51 +0530

    conf file for Payment details

---
+++ conf/payment_details_conf.py
@@ -0,0 +1,10 @@
+"""
+Conf file for Payment details form
+"""
+
+email = "sravanti.t@gmail.com"
+card_number = "4000007020000003"
+card_expiry = "0821"
+card_cvc = "123"
+zip_code = "560072"
+phone_number = "123456789"
