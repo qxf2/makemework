be96c05b009996bca60b01457f836dfac05b0262
Qxf2 <mak@qxf2.com>
2019-11-20 17:43:35 +0530

    Deleted the files

--- conf/e2e_weather_shopper_conf.py
+++
@@ -1,4 +0,0 @@
-"""
-This conf contains the data needed by test_e2e_purchase_product.py
-"""
-PURCHASE_LOGIC = {"moisturizer":['aloe'],"sunscreen":['spf-50']}
--- conf/testrailenv_conf.py
+++
@@ -1,7 +0,0 @@
-"""
-Conf file to hold the TestRail url and credentials
-"""
-
-testrail_url = "Add your testrail url"
-testrail_user = "Add your testrail username"
-testrail_password = "Add your testrail password"
